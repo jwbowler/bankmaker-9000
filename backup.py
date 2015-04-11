bf3d493cec7fc20bc44497d4d7b70088232ace9f
Jeffrey Sun <jeffreysun@Jeffreys-MacBook-Pro.local>
2015-04-11 12:54:17 -0400

    hopefully socket streams better now

--- backup.py
+++ backup.py
@@ -83,13 +83,13 @@ class Portfolio:
     def hello(self): #MUST ISSUE FIRST!!
     
         request = jsonify({\
-            "type": "hello", \
-            "team": TEAM_NAME })
-        s.send(request)
+                "type": "hello", \
+                "team": TEAM_NAME })
+        mysocket.mysend(request)
         print "Sent request"
-	res = s.recv(BUFFER_SIZE)
-	print "Response:", res
-	return res
+        res = mysocket.myreceive(BUFFER_SIZE)
+        print "Response:", res
+        return res
     
     
     def buy(self, symbol, price, size): 
@@ -101,8 +101,8 @@ class Portfolio:
             "price": price, \
             "size": size})
         self.numrequests += 1
-        s.send(request)
-        return s.recv(json.loads(BUFFER_SIZE))
+        mysocket.mysend(request)
+        return mysocket.myreceive(json.loads(BUFFER_SIZE))
             
         
     def sell(self, symbol, price, size):
@@ -114,8 +114,8 @@ class Portfolio:
             "price": price, \
             "size": size})
         self.numrequests += 1
-        s.send(request)
-        return s.recv(json.loads(BUFFER_SIZE))
+        mysocket.mysend(request)
+        return mysocket.myreceive(json.loads(BUFFER_SIZE))
     
     def convert(self, dir, size):
         request = jsonify({\
@@ -125,8 +125,8 @@ class Portfolio:
             "dir": dir, \
             "size": size})
         self.numrequests += 1
-        s.send(request)
-        return s.recv(json.loads(BUFFER_SIZE))
+        mysocket.mysend(request)
+        return mysocket.myreceive(json.loads(BUFFER_SIZE))
         
       # fixed cost of 100 per conversion (regardless of size)
       # one CORGE = 0.3 FOO + 0.8 BAR
@@ -136,8 +136,8 @@ class Portfolio:
         request = jsonify({\
             "type": "cancel", \
             "order_id": order_id})
-        s.send(request)
-        s.recv(json.loads(BUFFER_SIZE))
+        mysocket.mysend(request)
+        mysocket.myreceive(json.loads(BUFFER_SIZE))
       # returns OUT even if order_id is invalid
 
         
@@ -184,6 +184,40 @@ def handle(message):
         
 def jsonify(p):
     return json.dumps(p) + '\n'
+
+class mysocket:
+    '''demonstration class only
+      - coded for clarity, not efficiency
+    '''
+
+    def __init__(self, sock=None):
+        if sock is None:
+            self.sock = socket.socket(
+                socket.AF_INET, socket.SOCK_STREAM)
+        else:
+            self.sock = sock
+
+    def connect(self, host, port):
+        self.sock.connect((host, port))
+
+    def mysend(self, msg):
+        totalsent = 0
+        while totalsent < MSGLEN:
+            sent = self.sock.send(msg[totalsent:])
+            if sent == 0:
+                raise RuntimeError("socket connection broken")
+            totalsent = totalsent + sent
+
+    def myreceive(self):
+        chunks = []
+        bytes_recd = 0
+        while bytes_recd < MSGLEN:
+            chunk = self.sock.recv(min(MSGLEN - bytes_recd, 2048))
+            if chunk == '':
+                raise RuntimeError("socket connection broken")
+            chunks.append(chunk)
+            bytes_recd = bytes_recd + len(chunk)
+        return ''.join(chunks)
 
 if __name__ == '__main__':
     
