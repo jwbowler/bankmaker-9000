52617a802d258409b0cc046220ded9fbfdf53642
Jeffrey Sun <jeffreysun@Jeffreys-MacBook-Pro.local>
2015-04-11 14:56:18 -0400

    typo

--- backup.py
+++ backup.py
@@ -310,7 +310,7 @@ if __name__ == '__main__':
     portfolio.buy("BAR", 100, 100)
     portfolio.cancel(0)
 
-    for i in xrange(5)
+    for i in xrange(5):
         time.sleep(0.1)
         s.recv()
 
